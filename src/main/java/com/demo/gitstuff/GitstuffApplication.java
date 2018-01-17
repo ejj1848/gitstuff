b0a79b533beb624e7beb54aab9517b5b70309047
Eric.Johnson <eric.johnson@astontech.com>
2018-01-17 15:22:58 -0600

    Initial commit

---
+++ src/main/java/com/demo/gitstuff/GitstuffApplication.java
@@ -0,0 +1,12 @@
+package com.demo.gitstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitstuffApplication.class, args);
+	}
+}
